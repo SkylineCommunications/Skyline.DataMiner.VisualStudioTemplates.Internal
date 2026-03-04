6547ebb1b35fe5a3aae986b48391e407190aba52
Michiel Oda <97458010+MichielOda@users.noreply.github.com>
2026-03-04 09:04:36 +0100

    BPA template (#41)
    * BPA template
    
    (cherry picked from commit c9125e9ecb4ac0148aa495ea290a823f4516524e)
    
    * Update working/templates/bpa-solution/$BPA_PROJECT_NAME$/Documentation.md
    
    Co-authored-by: Wouter Demuynck <104209549+wdm-slc@users.noreply.github.com>
    (cherry picked from commit cb0d2344f316bfe31c256f5b04c86848bb816498)
    
    * Apply suggestions from code review
    
    Co-authored-by: Michiel Oda <97458010+MichielOda@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Michiel Oda <97458010+MichielOda@users.noreply.github.com>
    
    * Apply suggestion from @MichielOda
    
    Co-authored-by: Michiel Oda <97458010+MichielOda@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Michiel Oda <97458010+MichielOda@users.noreply.github.com>
    
    * CR remark updates
    
    * Small tweaks
    
    ---------
    
    Co-authored-by: Pedro Debevere <pedro.debevere@skyline.be>
    Co-authored-by: Pedro Debevere <97611683+PedroDebevere@users.noreply.github.com>
    Co-authored-by: Wouter Demuynck <104209549+wdm-slc@users.noreply.github.com>

---
+++ working/templates/bpa-solution/$BPA_PROJECT_NAME$/$BPA_PROJECT_NAME$TestEntry.cs
@@ -0,0 +1,81 @@
+﻿namespace $CLASS_NAME_PLACEHOLDER$Bpa
+{
+	using System;
+	using Skyline.DataMiner.BpaLib;
+
+	/// <summary>
+	/// $BPA_DESCRIPTION$
+	/// </summary>
+#if (IsCorrectiveBpa)
+	public class $CLASS_NAME_PLACEHOLDER$TestEntry : ABpaCorrectiveTest
+#else
+	public class $CLASS_NAME_PLACEHOLDER$TestEntry : ABpaTest
+#endif
+	{
+		public override string Name => "$BPA_PROJECT_NAME$";
+		public override string Description => "$BPA_DESCRIPTION$";
+		public override string Author => "$BPA_AUTHOR$";
+
+		// This class is where the test actually happens, all test code should go here.
+
+		// see the BpaTests/Examples solution for more options that can be overridden:
+		// - ExecuteConditions (e.g. Cassandra, Failover, ...)
+		// - Flags (e.g. CanRunOnOfflineAgent or RequireSLNet)
+		// - MaxVersions
+		// - MinVersions
+		// - RunMode (e.g. Cluster)
+		// - DefaultSchedule (BPA test will run every x time)
+
+		/// <summary>
+		/// This is the main entry point of the test.
+		/// When the test is run this function will be called.
+#if (IsCorrectiveBpa)
+		/// The result is then used to decide if <see cref="CorrectiveAction"/> will be called.
+#endif
+		/// </summary>
+		/// <param name="context">The execution context.</param>
+		/// <returns>The result of the test execution.</returns>
+#if (IsCorrectiveBpa)
+		protected override ABpaTestResult Verify(BpaExecuteContext context)
+#else
+		public override ABpaTestResult Run(BpaExecuteContext context)
+#endif
+		{
+			// Your test code goes here.
+			// For more information on how to implement a BPA, refer to https://aka.dataminer.services/BPADev.
+			// For more information on how to run a BPA in the standalone executor, refer to https://aka.dataminer.services/BPAStandaloneExecutor.
+
+			return new $CLASS_NAME_PLACEHOLDER$Result
+			{
+				// At the very least, provide an outcome and a result message
+				Outcome = BpaTestOutcome.NoIssues,
+				ResultMessage = "No issues detected",
+
+				// For issues, it is advised to provide an impact and info on corrective actions in the result
+				Impact = String.Empty,
+				CorrectiveAction = String.Empty,
+
+				// A JSON serializable object can be provided with further details.
+				// - In its simplest format, this is a string.
+				// - A list of string can be provided through:
+				//      BpaResultHelper.BuildStringListDetails(string[])
+				// - A custom JSON object tree can also be provided.
+				// See the examples in BpaTests/Examples for more info
+				DetailedJsonResult = "Detailed results",
+			};
+		}
+#if (IsCorrectiveBpa)
+	
+		/// <summary>
+		/// This function will be called if the verify function fails and the BPA is allowed to perform corrective actions
+		/// </summary>
+		/// <param name="context">The execution context.</param>
+		/// <returns>Indication of whether the corrective action succeeded.</returns>
+		protected override bool CorrectiveAction(BpaExecuteContext context)
+		{
+			// Your code for correcting the issue goes here.
+			return true;
+		}
+#endif
+	}
+}
